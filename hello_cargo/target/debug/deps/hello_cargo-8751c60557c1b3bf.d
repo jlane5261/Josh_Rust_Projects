cbc95d0c8bdbaaef7789003d599b911129ffca2f
Josh Lane <jlane5261@gmail.com>
2022-03-28 22:48:21 -0500

    First project using Cargo Package manager

---
+++ hello_cargo/target/debug/deps/hello_cargo-8751c60557c1b3bf.d
@@ -0,0 +1,5 @@
+/Users/josh/Coding/Josh_Rust_Projects/hello_cargo/target/debug/deps/hello_cargo-8751c60557c1b3bf: src/main.rs
+
+/Users/josh/Coding/Josh_Rust_Projects/hello_cargo/target/debug/deps/hello_cargo-8751c60557c1b3bf.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/josh/Coding/Josh_Rust_Projects/hello_cargo/target/debug/hello_cargo: /Users/josh/Coding/Josh_Rust_Projects/hello_cargo/src/main.rs
